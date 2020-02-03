e2667fa2900af856b25ab15e4d8e73246b598d33
tondi <mtondos2@gmail.com>
2020-02-03 23:04:55 +0100

    add generating projects from json, fix urls

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,49 @@ var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
 
 var plumberErrorHandler = {
-  errorHandler: notify.onError({
-    title: 'Gulp',
-    message: 'Error: <%= error.message %>'
+    errorHandler: notify.onError({
+        title: 'Gulp',
+        message: 'Error: <%= error.message %>'
 
-  })
+    })
 };
 
 // TODO unify copied code
 gulp.task('_sass', function() {
-  gulp.src('./scss/style.scss')
-    .pipe(plumber(plumberErrorHandler))
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('.'))
-    // no livereload
+    gulp.src('./scss/style.scss')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('.'))
+        // no livereload
 })
 
 gulp.task('sass', function() {
-  gulp.src('./scss/style.scss')
-    .pipe(plumber(plumberErrorHandler))
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('.'))
-    .pipe(livereload());
+    gulp.src('./scss/style.scss')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('.'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./scss/**/*.scss', ['sass']);
+    livereload.listen();
+    gulp.watch('./scss/**/*.scss', ['sass']);
 });
 
 // build
 gulp.task('copy', function() {
-  gulp.src(['index.html', 'style.css'])
-  .pipe(gulp.dest('build'))
+    gulp.src(['index.html', 'style.css'])
+        .pipe(gulp.dest('build'))
 
-  gulp.src('assets/*').pipe(gulp.dest('build/assets'));
+    gulp.src('assets/*').pipe(gulp.dest('build/assets'));
 });
 
 gulp.task('build', ['_sass', 'copy']);
